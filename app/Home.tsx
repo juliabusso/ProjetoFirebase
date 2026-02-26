0b3bc668abcdb894f24783d01b7382ce5030301a
juliabusso <juliabusso@hotmail.com>
2026-02-26 11:47:02 -0300

    Home com excluir conta realizado

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,6 +1,9 @@
 import { Text,StyleSheet,View,Button } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
+import {auth} from "../services/firebaseConfig"
+import { deleteUser } from "firebase/auth";
+import { Alert } from "react-native";
 
 export default function Home() {
     const router = useRouter()//Hook de navegação
@@ -9,10 +12,41 @@ export default function Home() {
         await AsyncStorage.removeItem("@user")//Limpa o usuário do Async
         router.replace("/")
     }
+const excluirConta = () => {
+    Alert.alert(
+        "ATENÇÃO",
+        "Deseja realmente excluir sua conta? Essa ação é irreversível!",
+        [
+            { text: "Cancelar", style: "cancel" },
+            {
+                text: "Excluir",
+                onPress: async () => {
+                    try {
+                        const user = auth.currentUser;
+
+                        if (user) {
+                            await deleteUser(user);
+                            await AsyncStorage.removeItem("@user"); // Limpa o usuário do Async
+                            Alert.alert("Sucesso,", "Conta excluída com sucesso!");
+                            router.replace("/");
+                        } else {
+                            Alert.alert("Erro", "Nenhum usuário logado.");
+                        }
+
+                    } catch (error) {
+                        console.log("Erro ao excluir conta: ", error);
+                        Alert.alert("Erro", "Não foi possível excluir a conta. Tente novamente mais tarde.");
+                    }
+                }
+            }
+        ]
+    );
+};
     return (
         <View style={styles.main}>
             <Text>Tela Home</Text>
             <Button title="Realizar logoff" onPress={realizarLogoff}/>
+            <Button title="Excluir conta" onPress={excluirConta} color="red"/>
         </View>
 
     )
